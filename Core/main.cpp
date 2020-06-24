838551f61ed611aebb5e3987daabe1adb4396a84
fahadmirza <fahadmirza8@gmail.com>
2020-06-24 05:22:20 -0500

    Let's now add all the initialization for the TensorFlow.

--- Core/main.cpp
+++ Core/main.cpp
@@ -8,11 +8,31 @@
 
 /* Includes ------------------------------------------------------------------*/
 #include "stm32746g_discovery.h"
+#include "sine_model.h"
+#include "tensorflow/lite/micro/kernels/all_ops_resolver.h"
+#include "tensorflow/lite/micro/micro_error_reporter.h"
+#include "tensorflow/lite/micro/micro_interpreter.h"
+#include "tensorflow/lite/schema/schema_generated.h"
+#include "tensorflow/lite/version.h"
 
 /* Private typedef -----------------------------------------------------------*/
 /* Private define ------------------------------------------------------------*/
 /* Private macro -------------------------------------------------------------*/
 /* Private variables ---------------------------------------------------------*/
+namespace
+{
+tflite::ErrorReporter* error_reporter = nullptr;
+const tflite::Model* model = nullptr;
+tflite::MicroInterpreter* interpreter = nullptr;
+TfLiteTensor* input = nullptr;
+TfLiteTensor* output = nullptr;
+
+// Create an area of memory to use for input, output, and intermediate arrays.
+// Finding the minimum value for your model may require some trial and error.
+constexpr uint32_t kTensorArenaSize = 2 * 1024;
+uint8_t tensor_arena[kTensorArenaSize];
+}// namespace
+
 // UART handler declaration
 UART_HandleTypeDef DebugUartHandler;
 
@@ -31,6 +51,9 @@ static void uart1_init(void);
   */
 int main(void)
 {
+    // Enable the CPU Cache
+	cpu_cache_enable();
+
     // Reset of all peripherals, Initializes the Flash interface and the Systick.
     HAL_Init();
 
@@ -42,6 +65,41 @@ int main(void)
 
     // Initialize UART1
     uart1_init();
+
+  	static tflite::MicroErrorReporter micro_error_reporter;
+  	error_reporter = &micro_error_reporter;
+
+  	// Map the model into a usable data structure. This doesn't involve any
+  	// copying or parsing, it's a very lightweight operation.
+    model = tflite::GetModel(sine_model);
+
+  	if(model->version() != TFLITE_SCHEMA_VERSION)
+  	{
+  		TF_LITE_REPORT_ERROR(error_reporter,
+  	                         "Model provided is schema version %d not equal "
+  	                         "to supported version %d.",
+  	                         model->version(), TFLITE_SCHEMA_VERSION);
+  	    return 0;
+  	}
+
+  	// This pulls in all the operation implementations we need.
+  	static tflite::ops::micro::AllOpsResolver resolver;
+
+  	// Build an interpreter to run the model with.
+  	static tflite::MicroInterpreter static_interpreter(model, resolver, tensor_arena, kTensorArenaSize, error_reporter);
+  	interpreter = &static_interpreter;
+
+  	// Allocate memory from the tensor_arena for the model's tensors.
+  	TfLiteStatus allocate_status = interpreter->AllocateTensors();
+  	if (allocate_status != kTfLiteOk)
+  	{
+  	    TF_LITE_REPORT_ERROR(error_reporter, "AllocateTensors() failed");
+  	    return 0;
+  	}
+
+  	// Obtain pointers to the model's input and output tensors.
+  	input = interpreter->input(0);
+  	output = interpreter->output(0);
 
  
     while (1)
