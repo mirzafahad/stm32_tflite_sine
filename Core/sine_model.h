206f19f1b3447f40b7d03610158631582b75c1cc
fahadmirza <fahadmirza8@gmail.com>
2020-06-24 04:18:35 -0500

    Added the TFLite model files downloaded from Google Colab;

---
+++ Core/sine_model.h
@@ -0,0 +1,15 @@
+
+
+
+// This is a TensorFlow Lite model file that has been converted into a C data
+// array using the tensorflow.lite.util.convert_bytes_to_c_source() function.
+// This form is useful for compiling into a binary for devices that don't have a
+// file system.
+
+#ifndef TENSORFLOW_LITE_UTIL_SINE_MODEL_DATA_H_
+#define TENSORFLOW_LITE_UTIL_SINE_MODEL_DATA_H_
+
+extern const unsigned char sine_model[];
+extern const int sine_model_len;
+
+#endif  // TENSORFLOW_LITE_UTIL_SINE_MODEL_DATA_H_
